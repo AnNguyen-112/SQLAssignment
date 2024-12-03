1d35d5c2ed790bc43f2cca26291b4e3dda4b1826
annguyen <annguyen01122000@gmail.com>
2024-12-03 01:53:52 -0600

    Assignment 3 2nd push

--- Assignment03.sql
+++ Assignment03.sql
@@ -43,20 +43,63 @@ JOIN [Order Details] OD ON o.OrderID = od.OrderID
 GROUP BY c.City
 
 --5.      List all Customer Cities that have at least two customers.
+SELECT DISTINCT c1.City
+FROM Customers c1
+JOIN Customers c2 
+    ON c1.City = c2.City 
+   AND c1.CustomerID < c2.CustomerID;
+--or
+
+SELECT c.City, Count(c.CustomerID) as NumberOfCustomers
+FROM Customers c
+GROUP BY City
+HAVING COUNT (c.CustomerID) >= 2
+
+
 
 
 --6.      List all Customer Cities that have ordered at least two different kinds of products.
+SELECT c.City, Count(od.ProductID) as [Number of Ordered Products]
+FROM Customers c JOIN Orders o On c.CustomerID = o.CustomerID
+JOIN [Order Details] od ON o.OrderID = od.OrderID
+GROUP By c.City
+Having Count(od.ProductID) > 2
+Order By Count(od.ProductID) ASC
 
---7.      List all Customers who have ordered products, but have the ‘ship city’ on the order different from their own customer cities.
 
---8.      List 5 most popular products, their average price, and the customer city that ordered most quantity of it.
+--7.  List all Customers who have ordered products, but have the ‘ship city’ on the order different from their own customer cities.
+SELECT Distinct c.ContactName, c.City as City, o.ShipCity as ShippingCity
+FROM Customers c Join Orders o On c.CustomerID = o.CustomerID
+WHERE c.City != o.ShipCity
+ 
+
+
+
+
+
+--8. List 5 most popular products, their average price, and the customer city that ordered most quantity of it.
+
+
 
 --9.      List all cities that have never ordered something but we have employees there.
-
 --a.      Use sub-query
+SELECT CITY
+FROM EMPLOYEES
+Where City Not in
+(
+	SELECT City
+	FROM Customers
+)
 
 --b.      Do not use sub-query
+SELECT e.City as City
+From Employees e Left Join Customers c ON e.City = c.City
+WHERE c.City is null
 
---10.  List one city, if exists, that is the city from where the employee sold most orders (not the product quantity) is, and also the city of most total quantity of products ordered from. (tip: join  sub-query)
+--10.  List one city, if exists, that is the city from where the employee sold most orders (not the product quantity) is, 
+--and also the city of most total quantity of products ordered from. (tip: join  sub-query)
+
+
 
 --11. How do you remove the duplicates record of a table?
+
